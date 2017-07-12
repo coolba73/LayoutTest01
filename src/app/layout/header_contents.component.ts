69ee9b6a7cf083bf5a1ddf8a760b42688453f749
coolba73 <coolba73@outlook.com>
2017-07-12 12:29:32 +0900

    1

--- src/app/layout/header_contents.component.ts
+++ src/app/layout/header_contents.component.ts
@@ -4,6 +4,7 @@ import { Component } from "@angular/core";
     selector : 'app-root',
     templateUrl : './header_contents.component.html',
     styleUrls : ['./header_contents.component.css']
+    
 })
 export class Header_Contents_Component{
     
