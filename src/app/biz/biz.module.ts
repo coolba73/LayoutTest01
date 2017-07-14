c850974638b8131591786a1fc0074dd67451207d
coolba73 <coolba73@outlook.com>
2017-07-14 16:18:47 +0900

    1

--- src/app/biz/biz.module.ts
+++ src/app/biz/biz.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from "@angular/core";
 import { MyTaskLayoutComponent } from "./mytasklayout.component";
 import { BizRouting } from "./biz.routing";
 
+import { 
+    MdCardModule 
+    ,MdButtonModule
+} from "@angular/material";
+
 @NgModule({
     imports : [
         BizRouting
+        ,MdCardModule
+        ,MdButtonModule
     ],
     declarations : [
         MyTaskLayoutComponent
